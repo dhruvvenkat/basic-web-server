82081904b6d7f01d5b079c272bfd85b76131b5d9
dvenkat <dhruv.venkat.2024@gmail.com>
2025-04-06 01:37:13 -0400

    first commit, returns basic info about client server

---
+++ server.py
@@ -0,0 +1,47 @@
+import http.server
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    '''Returning a fixed page to handle HTTP requests'''
+    
+    Page = f'''\
+        <html>
+        <body>
+        <table>
+            <tr> <td>Header</td>    <td>Value</td> </tr>
+            <tr> <td>Date & Time</td>    <td>{date_time}</td> </tr>
+            <tr> <td>Client host</td>    <td>{client_host}</td> </tr>
+            <tr> <td>Client port</td>    <td>{client_port}</td> </tr>
+            <tr> <td>Command</td>    <td>{command}</td> </tr>
+            <tr> <td>Path</td>    <td>{path}</td> </tr>
+        </table>
+        </body>
+        </html>
+    '''
+    def do_GET(self):
+        page = self.create_page()
+        self.send_page(page)
+
+    def create_page(self):
+        values = {
+            'date_time' : self.date_time_string(),
+            'client_host' : self.client_address[0],
+            'client_port' : self.client_address[1],
+            'command' : self.command,
+            'path' : self.path
+        }
+        page = self.Page.format(**values)
+        return page
+
+
+    #handling get requests
+    def send_page(self, page):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(bytes(self.Page, 'utf-8'))
+
+if __name__ == '__main__':
+    serverAddress = ('', 7000)
+    server = http.server.HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
